63f0c610dc86cd8170b249cdb6b3c09994cd19f5
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-04 16:26:46 -0500

    custom Hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Formulario.module.css'
+import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnología'}
+    ]
+
+    //utilizar custom Hook
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
     return (
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+                    <SelectNoticias />
 
                     <div className="input-field col s12">
                         <input 
